7687571aab1dedab146e7ca139514dbea5625823
MMcClure11 <mcclme02@luther.edu>
2020-07-13 10:04:35 -0500

    adds search method to student class

--- app/models/student.rb
+++ app/models/student.rb
@@ -14,7 +14,7 @@ class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
 
-  def search
-
+  def self.search(student)
+    self.where("name LIKE ?", "%#{student}%")
   end
 end
