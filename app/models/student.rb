d78a825d081f3a2e0656043f53287786f5b0ff0a
aredmond85 <aredmond85@gmail.com>
2020-07-24 11:59:28 -0500

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,11 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+  validates :name, presence: true
+  validates :hometown, presence: true
+  validates :birthday, presence: true
+
+  def self.search(query)
+    self.where("name LIKE ?", "%#{query}%")
+  end	  
 end
